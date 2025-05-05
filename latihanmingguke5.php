c24f6bc48a4e5ea25fc88dd387e2c4e76fbd3bec
farras366 <yascahya06@gmail.com>
2025-05-05 09:24:39 +0800

    Add files via upload

---
+++ latihanmingguke5.php
@@ -0,0 +1,11 @@
+<HTML>
+    <HEAD>
+        <TITLE>Contoh</TITLE>
+    </HEAD>
+    <BODY>
+        Selamat Belajar PHP. <BR>
+        <?php
+            Printf("Tgl. Sekarang : %s", Date("d F Y"))
+        ?>
+    </BODY>
+</HTML>
